1722257e4e0b2d313aed564ef5c53ae940de7a13
Anastasiya <nastyastud123@gmail.com>
2026-05-03 17:52:32 +0300

    Добавлен графический интерфейс для анализа зарплат и ввода N лет

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import PyQt5
+dirname = os.path.dirname(PyQt5.__file__)
+plugin_path = os.path.join(dirname, 'Qt5', 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,
+                             QVBoxLayout, QPushButton, QLineEdit, QLabel)
+
+class SalaryTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout(self)
+        layout.addWidget(QLabel("Прогноз на N лет (экстраполяция):"))
+        self.n_input = QLineEdit("3")
+        layout.addWidget(self.n_input)
+        self.btn_calc = QPushButton("Загрузить данные и построить графики")
+        layout.addWidget(self.btn_calc)
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        layout.addWidget(self.canvas)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Анализ зарплат в РФ (Вариант 8)")
+        self.setCentralWidget(SalaryTab())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
